e4e76283df9e0a6de21c8a7bd6c96f2d0323ad1f
lcc <liuchuanchuan@jsinfor.com>
2019-08-01 17:35:12 +0800

    二帮卖组件

---
+++ src/components/secondBand/sameMonth.vue
@@ -0,0 +1,149 @@
+<template>
+	<div class="sameMonth">
+		<div class="sameMonthcontent">
+			<div class="title">
+			   <div class="titleborder"></div>
+			   <div class="titletext">当月</div>
+			</div>
+			<div class="record">
+				<a-Row class="recordConten">
+				  <a-Col :span="14" class="recordLeft">
+				  	<div><a-icon type="pay-circle" />下单金额（万元）</div>
+					<div>(以创建订单日期统计)</div>
+					<div><span>￥</span>385<span>.08</span></div>
+				  </a-Col>
+				  <a-Col :span="10" class="recordRight">
+				  	<div>毛利额（万元）</div>
+					<div><span>￥</span>81.13</div>
+					<div>毛利率</div>
+					<div>13.24%</div>
+				  </a-Col>
+				</a-Row>
+			</div>
+			<div class="proportion">
+				
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		name:'samaMonth',
+		components:{
+
+		},
+		props:{
+		},
+		data(){
+			return {
+
+			}
+		},
+		mounted(){
+
+		},
+		methods(){
+
+		},
+	}
+</script>
+<style lang="less" scoped>
+	.sameMonth {
+		width: 100%;
+		padding: 5px;
+		background:rgba(255,255,255,1);
+		border-radius:10px; 
+		margin-bottom: 20px;
+		.sameMonthcontent {
+			width: 100%;
+			height: 545px;
+			border:4px solid #00A6FF;
+			border-radius:10px;
+			padding: 0 20px;
+			.title {
+				height: 50px;
+				display: flex;
+	    		align-items: center;
+	    		.titleborder {
+	    			width:12px;
+					height:4px;
+					background:rgba(45,146,252,1);
+					border-radius:2px;
+	    		}
+	    		.titletext {
+	    			padding-left: 5px;
+	    			font-size:16px;
+					font-family:PingFangSC-Medium;
+					font-weight:500;
+					color:rgba(51,51,51,1);
+	    		}
+			}
+			.record {
+				width: 100%;
+				height: 145px;
+				.recordConten {
+					height: 100%;
+					width: 100%;
+					.recordLeft {
+						height: 100%;
+						background:linear-gradient(90deg,rgba(0,91,234,1),rgba(0,198,251,1));
+						border-radius:8px;
+						div {
+							font-family:PingFangSC-Regular;
+							font-weight:400;
+							color:rgba(255,255,255,1);		
+						}
+						div:nth-child(1) {
+							height: 35%;
+							font-size:20px;
+							display: flex;
+						    align-items: center;
+						    justify-content: center;
+						}
+						div:nth-child(2) {
+							height: 15%;
+							font-size:12px;
+							display: flex;
+						    align-items: center;
+						    justify-content: center;
+						}
+						div:nth-child(3) {
+							height: 50%;
+							font-size:56px;
+							text-align: center;
+							span {
+								font-size:36px;
+							}
+						}
+					}
+					.recordRight {
+						height: 100%;
+						div {
+							font-family:PingFangSC-Medium;
+							font-weight:500;
+							height: 25%;
+							display: flex;
+						    align-items: center;
+							color:rgba(152,162,180,1);
+							span {
+								font-size:12px;
+							}
+						}
+						div:nth-child(1),
+						div:nth-child(3) {
+							margin-left: 10%;
+						}
+						div:nth-child(2),
+						div:nth-child(4) {
+							font-size:20px;
+							font-family:PingFangSC-Medium;
+							font-weight:bold;
+							color:rgba(51,51,51,1);
+						    justify-content: center;
+						}
+					}
+				}
+			}
+		}
+	}
+</style>
