c237f49db1fdbb8ebb85786a58555896b08b8e14
lcc <liuchuanchuan@jsinfor.com>
2019-08-06 15:43:47 +0800

    弹出框

--- src/components/salesman/index.vue
+++ src/components/salesman/index.vue
@@ -4,6 +4,8 @@
 		<manager :explicit="explicit" :salesmanData="salesmanData"></manager>
 		<core :coretype="'业务员得分'" :coretext="100" :evaluate="'较好'"></core>
 		<trendChart  v-if="echartsShow" :trendChartClick="trendChartClick"></trendChart>
+		<glide  v-if="glideShow" :trendChartClick="glideClick"></glide>
+		<contribution   v-if="contributionShow" :trendChartClick="contributionClick"></contribution>
 	</div>
 </template>
 <script>
@@ -11,13 +13,17 @@
 	import manager from "./manager.vue"
 	import core from "./../core.vue"
 	import trendChart from "./../secondBand/trendChart.vue"
+	import glide from "./glide.vue"
+	import contribution from "./contribution.vue"
 	export default {
 		name:'index',
 		components:{
 			subTitle,
 			manager,
 			core,
-			trendChart
+			trendChart,
+			glide,
+			contribution
 		},
 		props:{
 			salesmanData:{
@@ -30,6 +36,8 @@
 				subtitlename:'业务员',
 				listing:[],
 				echartsShow:false,
+				glideShow:false,
+				contributionShow:false,
 			}
 		},
 		mounted(){
@@ -39,10 +47,20 @@
 			explicit(a){
 				if(a == '走势图'){
 					this.echartsShow = true;
+				}else if(a == '下滑人员'){
+					this.glideShow = true;
+				}else if(a == '达成与贡献'){
+					this.contributionShow = true;
 				}
 			},
 			trendChartClick(){
 				this.echartsShow = false;
+			},
+			glideClick(){
+				this.glideShow = false;
+			},
+			contributionClick(){
+				this.contributionShow = false;
 			}
 		},
 		computd:{
